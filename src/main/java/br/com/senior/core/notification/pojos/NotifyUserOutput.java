fdf69c785b5d9a39616ae5e14a64acf621f26c33
Sérgio Souza <sergio.lcs@gmail.com>
2020-03-16 14:20:32 -0300

    Client do serviço de notificação (#7)
    * Client do serviço de usuários e refatoração
    
    * Client do serviço de notificação
    
    * Javadoc do serviço de notificação

---
+++ src/main/java/br/com/senior/core/notification/pojos/NotifyUserOutput.java
@@ -0,0 +1,21 @@
+package br.com.senior.core.notification.pojos;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.experimental.FieldDefaults;
+
+/**
+ * Payload de saída do {@link br.com.senior.core.notification.NotificationClient#notifyUser(NotifyUserInput) notifyUser}
+ */
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class NotifyUserOutput {
+
+    /**
+     * Boolean indicating operation success
+     */
+    @NonNull
+    public Boolean ok;
+
+}
---
+++ src/main/java/br/com/senior/core/notification/pojos/UserNotificationKind.java
@@ -0,0 +1,12 @@
+package br.com.senior.core.notification.pojos;
+
+/**
+ * Tipos de notificação
+ */
+public enum UserNotificationKind {
+
+    Operational,
+    Management,
+    News
+
+}
