e4fc2a713a5dde107e2ce0f11c81baba301bfced
Andre <meirezende@hotmail.com>
2020-03-19 09:32:08 -0300

    Update src/test/java/br/com/senior/core/authorization/AuthorizationIT.java
    Co-Authored-By: Sérgio Souza <sergio.lcs@gmail.com>

--- src/test/java/br/com/senior/core/authorization/AuthorizationIT.java
+++ src/test/java/br/com/senior/core/authorization/AuthorizationIT.java
@@ -21,7 +21,7 @@ public class AuthorizationIT extends BaseIT {
 
     @BeforeClass
     public static void beforeClass() throws ServiceException {
-        token = new AuthenticationClient().login(new LoginInput(System.getenv("SENIOR_USERNAME"), System.getenv("password_valid"))).getJsonToken().getAccess_token();
+        token = login().getJsonToken().getAccess_token();
     }
 
     @Test
