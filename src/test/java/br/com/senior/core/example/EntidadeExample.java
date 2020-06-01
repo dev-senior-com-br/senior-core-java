36b24cbb6436ca05fce1e23ee5c86e856835fab5
Lemoel Marques Vieira <lemoel@gmail.com>
2020-06-01 07:36:41 -0300

    Adicionado exemplo de uso da sdk com uma entidade (#22)

--- src/test/java/br/com/senior/core/example/EntidadeExample.java
+++ src/test/java/br/com/senior/core/example/EntidadeExample.java
@@ -8,7 +8,9 @@ import br.com.senior.core.entities.EntitiesUtils;
 import br.com.senior.core.entities.EntityPage;
 import br.com.senior.core.entities.FilterBuilder;
 import br.com.senior.core.example.pojos.Syndicate;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class EntidadeExample {
 
     public static void main(String[] args) throws ServiceException {
@@ -25,17 +27,21 @@ public class EntidadeExample {
         AuthenticationClient client = new AuthenticationClient();
         LoginOutput output = client.login(new LoginInput(username, password));
         String token = output.getJsonToken().getAccess_token();
+        log.info(token);
 
         //POST
         EntitiesUtils entityUtils = new EntitiesUtils<>("hcm", "payroll", token, Syndicate.class);
         syndicate = new Syndicate("4ca94ee8-a1c2-11ea-bb37-0242ad", 2L, "Nome do Sindicato", 12L);
-        syndicateResult = (Syndicate)entityUtils.executePost(ENTITY_NAME, syndicate);
+        syndicateResult = (Syndicate) entityUtils.executePost(ENTITY_NAME, syndicate);
+        log.info(syndicateResult.toString());
 
         //GET
         entityPage = entityUtils.executeGet(ENTITY_NAME, null);
+        log.info("Array com " + entityPage.getTotalElements() + " entidades");
 
         //GetById
-        syndicateResult = (Syndicate)entityUtils.executeGetById(ENTITY_NAME, "4ca94ee8-a1c2-11ea-bb37-0242ad");
+        syndicateResult = (Syndicate) entityUtils.executeGetById(ENTITY_NAME, "4ca94ee8-a1c2-11ea-bb37-0242ad");
+        log.info(syndicateResult.toString());
 
         //GET com Filter
         String filter = new FilterBuilder().field("id").equals("4ca94ee8-a1c2-11ea-bb37-0242ad").build();
@@ -44,6 +50,7 @@ public class EntidadeExample {
         //PUT
         Syndicate payload = new Syndicate("4ca94ee8-a1c2-11ea-bb37-0242ad", 2L, "Sindicato de tudo", 12L);
         syndicateResult = (Syndicate) entityUtils.executePut(ENTITY_NAME, "4ca94ee8-a1c2-11ea-bb37-0242ad", payload);
+        log.info(syndicateResult.toString());
 
         //Delete
         entityUtils.executeDelete(ENTITY_NAME, "4ca94ee8-a1c2-11ea-bb37-0242ad");
--- src/test/java/br/com/senior/core/example/pojos/Syndicate.java
+++ src/test/java/br/com/senior/core/example/pojos/Syndicate.java
@@ -2,10 +2,12 @@ package br.com.senior.core.example.pojos;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
+@Data
 public class Syndicate {
     String id;
     Long code;
